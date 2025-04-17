6d04b4e3dd8feb59282b3620412c08b6e12ec503
jananikolovska <jnikolovskaa@gmail.com>
2025-04-17 11:38:42 +0200

    [Project.ipynb] initial code, [gitignore, install.sh, README] replicability

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # LLMinStockPrediction
+
+### Replicability 
+
+#### Step 1: Create a `secrets` Folder
+
+In the root of your project directory, create a folder named `secrets`:
+
+```bash
+mkdir secrets
+```
+
+#### Step 2: Add the API Key
+
+Inside the `secrets` folder, create a file called `openai_key.txt`:
+
+```bash
+touch secrets/openai_key.txt
+```
+
+Paste your OpenAI API key into that file. It should look like this:
+
+```
+sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
---
+++ install.sh
@@ -0,0 +1,22 @@
+conda create -n llm_stock_prediciton python=3.9
+conda activate llm_stock_prediciton
+pip install numpy
+pip install -U jax[cpu] # we don't need GPU for jax
+pip install torch --index-url https://download.pytorch.org/whl/cu118
+pip install openai==0.28.1
+pip install tiktoken
+pip install tqdm
+pip install matplotlib
+pip install "pandas<2.0.0"
+pip install darts
+pip install gpytorch
+pip install transformers
+pip install datasets
+pip install multiprocess
+pip install SentencePiece
+pip install accelerate
+pip install gdown
+pip install dash
+pip install plotly
+pip install jupyter
+conda deactivate
